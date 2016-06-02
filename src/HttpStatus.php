ff63a31fb28f277cfa60e5d18e57563aa8a22ff5
daniel bannert <d.bannert@anolilab.de>
2016-06-02 22:10:54 +0200

    cs fixes

--- src/HttpStatus.php
+++ src/HttpStatus.php
@@ -2,7 +2,44 @@
 namespace Narrowspark\HttpStatus;
 
 use InvalidArgumentException;
-use Narrowspark\HttpStatus\Exception;
+use Narrowspark\HttpStatus\Exception\BadGatewayException;
+use Narrowspark\HttpStatus\Exception\BadRequestException;
+use Narrowspark\HttpStatus\Exception\ConflictException;
+use Narrowspark\HttpStatus\Exception\ExpectationFailedException;
+use Narrowspark\HttpStatus\Exception\FailedDependencyException;
+use Narrowspark\HttpStatus\Exception\ForbiddenException;
+use Narrowspark\HttpStatus\Exception\GatewayTimeoutException;
+use Narrowspark\HttpStatus\Exception\GoneException;
+use Narrowspark\HttpStatus\Exception\HttpVersionNotSupportedException;
+use Narrowspark\HttpStatus\Exception\ImATeapotException;
+use Narrowspark\HttpStatus\Exception\InsufficientStorageException;
+use Narrowspark\HttpStatus\Exception\InternalServerErrorException;
+use Narrowspark\HttpStatus\Exception\LengthRequiredException;
+use Narrowspark\HttpStatus\Exception\LockedException;
+use Narrowspark\HttpStatus\Exception\LoopDetectedException;
+use Narrowspark\HttpStatus\Exception\MethodNotAllowedException;
+use Narrowspark\HttpStatus\Exception\NetworkAuthenticationRequiredException;
+use Narrowspark\HttpStatus\Exception\NotAcceptableException;
+use Narrowspark\HttpStatus\Exception\NotExtendedException;
+use Narrowspark\HttpStatus\Exception\NotFoundException;
+use Narrowspark\HttpStatus\Exception\NotImplementedException;
+use Narrowspark\HttpStatus\Exception\PaymentRequiredException;
+use Narrowspark\HttpStatus\Exception\PreconditionFailedException;
+use Narrowspark\HttpStatus\Exception\PreconditionRequiredException;
+use Narrowspark\HttpStatus\Exception\ProxyAuthenticationRequiredException;
+use Narrowspark\HttpStatus\Exception\RequestedRangeNotSatisfiableException;
+use Narrowspark\HttpStatus\Exception\RequestEntityTooLargeException;
+use Narrowspark\HttpStatus\Exception\RequestHeaderFieldsTooLargeException;
+use Narrowspark\HttpStatus\Exception\RequestTimeoutException;
+use Narrowspark\HttpStatus\Exception\RequestUriTooLongException;
+use Narrowspark\HttpStatus\Exception\ServiceUnavailableException;
+use Narrowspark\HttpStatus\Exception\TooManyRequestsException;
+use Narrowspark\HttpStatus\Exception\UnauthorizedException;
+use Narrowspark\HttpStatus\Exception\UnavailableForLegalReasonsException;
+use Narrowspark\HttpStatus\Exception\UnprocessableEntityException;
+use Narrowspark\HttpStatus\Exception\UnsupportedMediaTypeException;
+use Narrowspark\HttpStatus\Exception\UpgradeRequiredException;
+use Narrowspark\HttpStatus\Exception\VariantAlsoNegotiatesException;
 use OutOfBoundsException;
 
 class HttpStatus
@@ -92,45 +129,44 @@ class HttpStatus
      * @var array
      */
     private static $phrasesExceptions = [
-        400 => Exception\BadRequestException::class,
-        401 => Exception\UnauthorizedException::class,
-        402 => Exception\PaymentRequiredException::class,
-        403 => Exception\ForbiddenException::class,
-        404 => Exception\NotFoundException::class,
-        405 => Exception\MethodNotAllowedException::class,
-        406 => Exception\NotAcceptableException::class,
-        407 => Exception\ProxyAuthenticationRequiredException::class,
-        408 => Exception\RequestTimeoutException::class,
-        409 => Exception\ConflictException::class,
-        410 => Exception\GoneException::class,
-        411 => Exception\LengthRequiredException::class,
-        412 => Exception\PreconditionFailedException::class,
-        413 => Exception\RequestEntityTooLargeException::class,
-        414 => Exception\RequestUriTooLongException::class,
-        415 => Exception\UnsupportedMediaTypeException::class,
-        416 => Exception\RequestedRangeNotSatisfiableException::class,
-        417 => Exception\ExpectationFailedException::class,
-        418 => Exception\ImATeapotException::class,
-        422 => Exception\UnprocessableEntityException::class,
-        423 => Exception\LockedException::class,
-        424 => Exception\FailedDependencyException::class,
-        425 => Exception\FailedDependencyException::class,
-        426 => Exception\UpgradeRequiredException::class,
-        428 => Exception\PreconditionRequiredException::class,
-        429 => Exception\TooManyRequestsException::class,
-        431 => Exception\RequestHeaderFieldsTooLargeException::class,
-        451 => Exception\UnavailableForLegalReasonsException::class,
-        500 => Exception\InternalServerErrorException::class,
-        501 => Exception\NotImplementedException::class,
-        502 => Exception\BadGatewayException::class,
-        503 => Exception\ServiceUnavailableException::class,
-        504 => Exception\GatewayTimeoutException::class,
-        505 => Exception\HttpVersionNotSupportedException::class,
-        506 => Exception\VariantAlsoNegotiatesException::class,
-        507 => Exception\InsufficientStorageException::class,
-        508 => Exception\LoopDetectedException::class,
-        510 => Exception\NotExtendedException::class,
-        511 => Exception\NetworkAuthenticationRequiredException::class,
+        400 => BadRequestException::class,
+        401 => UnauthorizedException::class,
+        402 => PaymentRequiredException::class,
+        403 => ForbiddenException::class,
+        404 => NotFoundException::class,
+        405 => MethodNotAllowedException::class,
+        406 => NotAcceptableException::class,
+        407 => ProxyAuthenticationRequiredException::class,
+        408 => RequestTimeoutException::class,
+        409 => ConflictException::class,
+        410 => GoneException::class,
+        411 => LengthRequiredException::class,
+        412 => PreconditionFailedException::class,
+        413 => RequestEntityTooLargeException::class,
+        414 => RequestUriTooLongException::class,
+        415 => UnsupportedMediaTypeException::class,
+        416 => RequestedRangeNotSatisfiableException::class,
+        417 => ExpectationFailedException::class,
+        418 => ImATeapotException::class,
+        422 => UnprocessableEntityException::class,
+        423 => LockedException::class,
+        424 => FailedDependencyException::class,
+        426 => UpgradeRequiredException::class,
+        428 => PreconditionRequiredException::class,
+        429 => TooManyRequestsException::class,
+        431 => RequestHeaderFieldsTooLargeException::class,
+        451 => UnavailableForLegalReasonsException::class,
+        500 => InternalServerErrorException::class,
+        501 => NotImplementedException::class,
+        502 => BadGatewayException::class,
+        503 => ServiceUnavailableException::class,
+        504 => GatewayTimeoutException::class,
+        505 => HttpVersionNotSupportedException::class,
+        506 => VariantAlsoNegotiatesException::class,
+        507 => InsufficientStorageException::class,
+        508 => LoopDetectedException::class,
+        510 => NotExtendedException::class,
+        511 => NetworkAuthenticationRequiredException::class,
     ];
 
     /**
